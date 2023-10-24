a40e11e3cb1a4bb1851c85f49a318ab99b7937e7
Piv94165 <alice.juan@live.fr>
2023-10-24 14:49:54 +0200

    return similarity to chien word

--- index.ts
+++ index.ts
@@ -1,11 +1,38 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
-function similarity(word1:string,word2:string) : string {
-	return word1+word2;
+async function similarity(wordToFind:string,guessedWord:string) : Promise<number> {
+	const body = {
+    sim1: wordToFind,
+    sim2: guessedWord,
+    lang: "fr",
+    type: "General Word2Vec",
+  };
+  const similarityResponse = await fetch(
+    "http://nlp.polytechnique.fr/similarityscore",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body),
+    }
+  );
+  const similarityResponseJson = await similarityResponse.json();
+  return Number(similarityResponseJson.simscore);
+}
+
+async function extractGuess(req: Request):Promise<string> {
+	const slackPayload = await req.formData();
+  const guess = await slackPayload.get("text")?.toString();
+  if (!guess) {
+    throw Error("Guess is empty or null");
+  }
+  return guess;
 }
 
 async function handler(_req: Request): Promise<Response> {
-	const result : string = similarity("toto","chat");
+	const guess = await extractGuess(_req);
+	const result = await String(similarity("chien",guess));
   return new Response(result);
 }
 
