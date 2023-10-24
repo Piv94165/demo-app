3021238b78969517cc14598a46fded0f5a5b42b7
Piv94165 <alice.juan@live.fr>
2023-10-24 13:38:01 +0200

    concatenate 2 words

--- index.ts
+++ index.ts
@@ -1,6 +1,11 @@
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 
+function similarity(word1:string,word2:string) : string {
+	return word1+word2;
+}
+
 async function handler(_req: Request): Promise<Response> {
+	const result : string = similarity("toto","chat");
   return new Response("Hello World");
 }
 
